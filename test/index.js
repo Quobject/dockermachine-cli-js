cf0e2f9ecb338ae51d6aa69b503281d868d11139
MatthiasL <matthias_ludwig@yahoo.com>
2015-09-30 09:34:16 +1000

    .

--- test/index.js
+++ test/index.js
@@ -158,84 +158,24 @@ describe('docker', function () {
   });
 
 
+  it('command ip should pass', function (done) {
+    this.timeout(1 * 60 * 1000);//1 minute
+    var dockerMachine = new DockerMachine();
+
+    assert.isNotNull(dockerMachine);
+    var failed = false;
+    var err = null;
+    dockerMachine.command('ip machinename').then(function (data) {
+      console.log('data = ', data);
+      assert.isNotNull(data);
+    }).finally(function () {
+      //console.log('finally ');
+      assert.isFalse(failed);
+      assert.isNull(err);
+      done();
+    });
+  });
+
 });
 
 
-
-
-//data =  { command: 'docker-machine inspect machinename ',                                                                      
-//  raw: '["{\\n    \\"ConfigVersion\\": 1,\\n    \\"Driver\\": {\\n        \\"IPAddress\\": \\"52.64.186.82\\",\\n        \\"SSH
-//User\\": \\"ubuntu\\",\\n        \\"SSHPort\\": 22,\\n        \\"MachineName\\": \\"machinename\\",\\n        \\"CaCertPath\\":
-// \\"/home/ubuntu/.docker/machine/certs/ca.pem\\",\\n        \\"PrivateKeyPath\\": \\"/home/ubuntu/.docker/machine/certs/ca-key.
-//pem\\",\\n        \\"SwarmMaster\\": false,\\n        \\"SwarmHost\\": \\"tcp://0.0.0.0:3376\\",\\n        \\"SwarmDiscovery\\"
-//: \\"\\",\\n        \\"Id\\": \\"3b185955ffdb942797c64c78af725cb5\\",\\n        \\"AccessKey\\": \\"PUTVALUEHERE\\",\\n
-//        \\"SecretKey\\": \\"abcdefPUTVALUEHERE\\",\\n        \\"SessionToken\\": \\"\\",\\n        \\"Reg
-//ion\\": \\"ap-southeast-2\\",\\n        \\"AMI\\": \\"ami-b59ce48f\\",\\n        \\"SSHKeyID\\": 0,\\n        \\"KeyName\\": \\
-//"machinename\\",\\n        \\"InstanceId\\": \\"i-d7c21808\\",\\n        \\"InstanceType\\": \\"t2.micro\\",\\n        \\"Priva
-//teIPAddress\\": \\"172.31.10.244\\",\\n        \\"SecurityGroupId\\": \\"sg-7db92718\\",\\n        \\"SecurityGroupName\\": \\"
-//docker-machine\\",\\n        \\"ReservationId\\": \\"\\",\\n        \\"RootSize\\": 8,\\n        \\"IamInstanceProfile\\": \\"\
-//\",\\n        \\"VpcId\\": \\"vpc-3413c051\\",\\n        \\"SubnetId\\": \\"subnet-5e44f13b\\",\\n        \\"Zone\\": \\"a\\",\
-//\n        \\"RequestSpotInstance\\": false,\\n        \\"SpotPrice\\": \\"0.50\\",\\n        \\"PrivateIPOnly\\": false,\\n    
-//    \\"Monitoring\\": false\\n    },\\n    \\"DriverName\\": \\"amazonec2\\",\\n    \\"HostOptions\\": {\\n        \\"Driver\\"
-//: \\"\\",\\n        \\"Memory\\": 0,\\n        \\"Disk\\": 0,\\n        \\"EngineOptions\\": {\\n            \\"ArbitraryFlags\
-//\": [],\\n            \\"Dns\\": null,\\n            \\"GraphDir\\": \\"\\",\\n            \\"Env\\": [],\\n            \\"Ipv6
-//\\": false,\\n            \\"InsecureRegistry\\": [],\\n            \\"Labels\\": [],\\n            \\"LogLevel\\": \\"\\",\\n 
-//           \\"StorageDriver\\": \\"\\",\\n            \\"SelinuxEnabled\\": false,\\n            \\"TlsCaCert\\": \\"\\",\\n   
-//         \\"TlsCert\\": \\"\\",\\n            \\"TlsKey\\": \\"\\",\\n            \\"TlsVerify\\": true,\\n            \\"Regis
-//  tryMirror\\": [],\\n            \\"InstallURL\\": \\"https://get.docker.com\\"\\n        },\\n        \\"SwarmOptions\\": {\\n 
-//           \\"IsSwarm\\": false,\\n            \\"Address\\": \\"\\",\\n            \\"Discovery\\": \\"\\",\\n            \\"M
-//  aster\\": false,\\n            \\"Host\\": \\"tcp://0.0.0.0:3376\\",\\n            \\"Image\\": \\"swarm:latest\\",\\n         
-//   \\"Strategy\\": \\"spread\\",\\n            \\"Heartbeat\\": 0,\\n            \\"Overcommit\\": 0,\\n            \\"TlsCaCer
-//  t\\": \\"\\",\\n            \\"TlsCert\\": \\"\\",\\n            \\"TlsKey\\": \\"\\",\\n            \\"TlsVerify\\": false,\\n
-//            \\"ArbitraryFlags\\": []\\n        },\\n        \\"AuthOptions\\": {\\n            \\"StorePath\\": \\"\\",\\n     
-//       \\"CaCertPath\\": \\"/home/ubuntu/.docker/machine/certs/ca.pem\\",\\n            \\"CaCertRemotePath\\": \\"\\",\\n     
-//       \\"ServerCertPath\\": \\"/home/ubuntu/.docker/machine/machines/machinename/server.pem\\",\\n            \\"ServerKeyPath
-//\\": \\"/home/ubuntu/.docker/machine/machines/machinename/server-key.pem\\",\\n            \\"ClientKeyPath\\": \\"/home/ubuntu
-///.docker/machine/certs/key.pem\\",\\n            \\"ServerCertRemotePath\\": \\"\\",\\n            \\"ServerKeyRemotePath\\": \
-//\"\\",\\n            \\"PrivateKeyPath\\": \\"/home/ubuntu/.docker/machine/certs/ca-key.pem\\",\\n            \\"ClientCertPath
-//\\": \\"/home/ubuntu/.docker/machine/certs/cert.pem\\"\\n        }\\n    },\\n    \\"StorePath\\": \\"/home/ubuntu/.docker/mach
-//              ine/machines/machinename\\"\\n}\\n",""]',                                                                                      
-//              machine:                                                                                                                     
-//                { ConfigVersion: 1,                                                                                                         
-//                  Driver:                                                                                                                   
-//                  { IPAddress: '52.64.186.82',                                                                                             
-//                    SSHUser: 'ubuntu',                                                                                                     
-//                    SSHPort: 22,                                                                                                           
-//                    MachineName: 'machinename',                                                                                            
-//                    CaCertPath: '/home/ubuntu/.docker/machine/certs/ca.pem',                                                               
-//                    PrivateKeyPath: '/home/ubuntu/.docker/machine/certs/ca-key.pem',                                                       
-//                    SwarmMaster: false,                                                                                                    
-//                    SwarmHost: 'tcp://0.0.0.0:3376',                                                                                       
-//                    SwarmDiscovery: '',                                                                                                    
-//                    Id: '3b185955ffdb942797c64c78af725caa',                                                                                
-//                    AccessKey: 'PUTVALUEHERE',                                                                                     
-//                    SecretKey: 'abcdefPUTVALUEHERE',                                                                 
-//                    SessionToken: '',                                                                                                      
-//                    Region: 'ap-southeast-2',                                                                                              
-//                    AMI: 'ami-b59ce48f',                                                                                                   
-//                    SSHKeyID: 0,                                                                                                           
-//                    KeyName: 'machinename',                                                                                                
-//                    InstanceId: 'i-d7c21808',                                                                                              
-//                    InstanceType: 't2.micro',                                                                                              
-//                    PrivateIPAddress: '172.31.10.211',                                                                                     
-//                    SecurityGroupId: 'sg-7db927aa',                                                                                        
-//                    SecurityGroupName: 'docker-machine',                                                                                   
-//                    ReservationId: '',                                                                                                     
-//                    RootSize: 8,                                                                                                           
-//                    IamInstanceProfile: '',                                                                                                
-//                    VpcId: 'vpc-3413c0aa',                                                                                                 
-//                    SubnetId: 'subnet-5e44f1aa',                                                                                           
-//                    Zone: 'a',                                                                                                             
-//                    RequestSpotInstance: false,                                                                                            
-//                    SpotPrice: '0.50',                                                                                                     
-//                    PrivateIPOnly: false,                                                                                                  
-//                    Monitoring: false },                                                                                                   
-//                  DriverName: 'amazonec2',                                                                                                  
-//                  HostOptions:                                                                                                              
-//                  { Driver: '',                                                                                                            
-//                    Memory: 0,                                                                                                             
-//                    Disk: 0,                                                                                                               
-//                    EngineOptions: [Object],                                                                                               
-//                    SwarmOptions: [Object],                                                                                                
-//                    AuthOptions: [Object] },                                                                                               
-//                  StorePath: '/home/ubuntu/.docker/machine/machines/machinename' } }                                                        
